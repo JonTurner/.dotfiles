81d88501a90de4bbee497e95910de2e9e74d6c74
JonTurner <JonTurner@users.noreply.github.com>
2023-08-13 23:54:08 -0400

    plugins cleanup after installing lazy

--- .config/nvim/lua/JT/plugins.lua
+++ .config/nvim/lua/JT/plugins.lua
@@ -13,32 +13,29 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
   {
-    'nvim-telescope/telescope.nvim', version = '0.1.0',
-    dependencies = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    dependencies = { 'nvim-lua/plenary.nvim' }
   },
   {
-	  'rose-pine/neovim',
-	  name = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
   },
   {
-      "folke/trouble.nvim",
-      config = function()
-          require("trouble").setup {
-              icons = false,
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end
+    "folke/trouble.nvim",
+    opts = {
+      icons = false,
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
   },
-
   {
-			'nvim-treesitter/nvim-treesitter',
-      build = ":TSUpdate"
-    },
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate"
+  },
   "nvim-treesitter/playground",
   "theprimeagen/harpoon",
   "theprimeagen/refactoring.nvim",
@@ -47,68 +44,59 @@ local plugins = {
   "nvim-treesitter/nvim-treesitter-context",
 
   {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  dependencies = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
-
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
-
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
   },
-
   "folke/zen-mode.nvim",
   "eandrju/cellular-automaton.nvim",
-
-  -- masks config file values with ********
-  -- "laytan/cloak.nvim",
-  -- configure here: after/plugin/cloak.lua
+  -- "laytan/cloak.nvim", masks config file values with ********
   "tpope/vim-rails",
   "tpope/vim-commentary",
-  -- This is just a rewrite of https://github.com/tpope/vim-endwise to leverage
-  -- Treesitter so it can be more accurate and work without having to run
-  -- Neovim's slow regex based highlighting along with nvim-treesitter highlighting.
-  "RRethy/nvim-treesitter-endwise",
-
+  "RRethy/nvim-treesitter-endwise", -- rewrite of tpope/vim-endwise
   "tpope/vim-repeat",
-
   "christoomey/vim-tmux-navigator",
   -- "morhetz/gruvbox",
   "github/copilot.vim",
   "AndrewRadev/ember_tools.vim",
   "tpope/vim-projectionist",
   {
-      "folke/which-key.nvim",
-      config = function()
-          vim.o.timeout = true
-          vim.o.timeoutlen = 300
-          require("which-key").setup {
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
   },
   "tpope/vim-rhubarb",
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
     end
   },
   --       Old text                    Command         New text
@@ -120,44 +108,6 @@ local plugins = {
   --     'change quot*es'            cs'"            "change quotes"
   --     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
   --     delete(functi*on calls)     dsf             function calls
-  -- {
-  --   "vimwiki/vimwiki",
-  --   init = function()
-  --     -- attempting to enable copilot in vimwiki
-  --     -- VimWiki was overriding the <TAB> complettion for copilot
-  --     vim.g.vimwiki_key_mappings = { table_mappings = 0, }
-  --   end,
-  --   config = function()
-  --     -- makes vimiwiki use markdown links as [text](text.md) instead of [text](text)
-  --     vim.g.vimwiki_markdown_link_ext = 1
-  --     vim.g.vimwiki_folding = ''
-  --     vim.g.vimwiki_list = {
-  --       {
-  --         path = '~/vimwiki/managed_billing',
-  --         syntax = 'markdown',
-  --         ext = '.md'
-  --       },
-  --       {
-  --         path = '~/vimwiki/notes',
-  --         syntax = 'markdown',
-  --         ext = '.md'
-  --       },
-  --       {
-  --         path = '~/vimwiki/investigations',
-  --         syntax = 'markdown',
-  --         ext = '.md'
-  --       }
-  --     }
-  --   end
-  -- },
-  -- {
-  --   "tools-life/taskwiki",
-  --   config = function()
-  --     vim.g.taskwiki_markup_syntax = 'markdown'
-  --     vim.g.taskwiki_filetypes = 'md'
-  --     vim.g.taskwiki_dont_fold = 1
-  --   end
-  -- },
   -- https://github.com/ryanoasis/nerd-fonts/#option-4-homebrew-fonts
   -- brew tap homebrew/cask-fonts
   -- brew install font-hack-nerd-font
@@ -167,16 +117,50 @@ local plugins = {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true }
   },
-  -- use{
-  --     "tbabej/taskwiki",
-  --     config = function()
-  --         vim.g.taskwiki_markup_syntax = 'markdown'
-  --     end
-  -- }
 }
 
--- rm -rf ~/.local/share/nvim/site/pack/packer/
--- rm -rf ~/.config/nvim/plugin/packer_compiled.lua
-
-
 require("lazy").setup(plugins)
+-- {
+--   "vimwiki/vimwiki",
+--   init = function()
+--     -- attempting to enable copilot in vimwiki
+--     -- VimWiki was overriding the <TAB> complettion for copilot
+--     vim.g.vimwiki_key_mappings = { table_mappings = 0, }
+--   end,
+--   config = function()
+--     -- makes vimiwiki use markdown links as [text](text.md) instead of [text](text)
+--     vim.g.vimwiki_markdown_link_ext = 1
+--     vim.g.vimwiki_folding = ''
+--     vim.g.vimwiki_list = {
+--       {
+--         path = '~/vimwiki/managed_billing',
+--         syntax = 'markdown',
+--         ext = '.md'
+--       },
+--       {
+--         path = '~/vimwiki/notes',
+--         syntax = 'markdown',
+--         ext = '.md'
+--       },
+--       {
+--         path = '~/vimwiki/investigations',
+--         syntax = 'markdown',
+--         ext = '.md'
+--       }
+--     }
+--   end
+-- },
+-- {
+--   "tools-life/taskwiki",
+--   config = function()
+--     vim.g.taskwiki_markup_syntax = 'markdown'
+--     vim.g.taskwiki_filetypes = 'md'
+--     vim.g.taskwiki_dont_fold = 1
+--   end
+-- },
+-- {
+--   "tbabej/taskwiki",
+--   config = function()
+--     vim.g.taskwiki_markup_syntax = 'markdown'
+--   end
+-- }
