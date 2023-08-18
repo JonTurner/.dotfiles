95af78a059e6e1d7057f6f265f25cfe0e21134e7
JonTurner <JonTurner@users.noreply.github.com>
2023-08-18 03:26:16 -0400

    add #ColorMyPencils to gruvbox config

--- .config/nvim/after/plugin/gruvbox.lua
+++ .config/nvim/after/plugin/gruvbox.lua
@@ -22,8 +22,19 @@ require("gruvbox").setup({
   dim_inactive = false,
   transparent_mode = false,
 })
-vim.cmd("colorscheme gruvbox")
+-- vim.cmd("colorscheme gruvbox")
 
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+
+function ColorMyPencils(color)
+	color = color or "gruvbox"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
