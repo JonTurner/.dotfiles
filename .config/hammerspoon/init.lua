ba2fd5d991fe710d4c0cefa65851303f400d5b79
JonTurner <JonTurner@users.noreply.github.com>
2024-12-31 18:24:48 -0500

    hammerspoon-wip - heading working for notion and linear

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -8,22 +8,30 @@ end)
 --------------------------------------
 -- Press cmd/ctrl + option/shift + 0 to create text.
 
--- local H1 = {{"cmd", "option"}, "1"} -- Asana,
--- local H1 = {{"cmd", "option", "shift", "ctrl"}, "1"} -- Asana,
--- local H1 = {{"cmd", "alt", "shift", "ctrl"}, "1"} -- Asana,
-local H1 = {{"cmd", "option"}, "1"} -- Asana,
--- local H2 = {{"cmd", "option"}, "2"}
--- local H3 = {{"cmd", "option"}, "3"}
--- local CHECKBOX = {{"cmd", "option"}, "4"}
--- local BULLET_LIST = {{"cmd", "option"}, "5"}
--- local NUMBERED_LIST = {{"cmd", "option"}, "6"}
--- local TOGGLE_LIST = {{"cmd", "option"}, "7"}
--- local CODE_BLOCK = {{"cmd", "option"}, "8"}
+-- local H1 = {{"cmd", "alt"}, "1"} -- Asana,
+-- local H2 = {{"cmd", "alt"}, "2"}
+-- local H3 = {{"cmd", "alt"}, "3"}
+-- local CHECKBOX = {{"cmd", "alt"}, "4"}
+-- local BULLET_LIST = {{"cmd", "alt"}, "5"}
+-- local NUMBERED_LIST = {{"cmd", "alt"}, "6"}
+-- local TOGGLE_LIST = {{"cmd", "alt"}, "7"}
+-- local CODE_BLOCK = {{"cmd", "alt"}, "8"}
 -- local COMMENT = {{"cmd", "shift"}, "M"}
 -- local STRIKETHROUGH = {{"cmd", "shift"}, "S"}
 
 local INLINE_CODE_BLOCK = {{"cmd"}, "E"}       -- for example, a shortcut to trigger an inline code block
 local COPY_URL = {{"cmd"}, "L"}
+-- made up shortcuts to hopefully avoid collisions
+-- local H1 = {{"cmd", "alt", "shift", "ctrl"}, "4"} -- Asana,
+-- local H2 = {{"cmd", "alt", "shift", "ctrl"}, "5"} -- Asana,
+-- local H3 = {{"cmd", "alt", "shift", "ctrl"}, "6"} -- Asana,
+-- local CHECKBOX = {{"cmd", "alt", "shift", "ctrl"}, "8"}
+-- made up shortcuts to hopefully avoid collisions
+local H1 = {{"cmd", "ctrl"}, "4"} -- Asana,
+local H2 = {{"cmd", "ctrl"}, "5"} -- Asana,
+local H3 = {{"cmd", "ctrl"}, "6"} -- Asana,
+local CHECKBOX = {{"cmd", "alt", "shift", "ctrl"}, "8"}
+
 
 -- Sample config table. Each key is an application name, and the value is a list of shortcuts.
 -- "listenShortcut" is the keystroke you want Hammerspoon to listen for;
@@ -31,7 +39,21 @@ local COPY_URL = {{"cmd"}, "L"}
 -- Add more as needed…
 
 local shortcutsConfig = {
-  Slack = {
+  Notion ={
+    {
+      listenShortcut = H1,
+      overwriteShortcut = {{"cmd", "alt"}, "1"}
+    },
+    {
+      listenShortcut = H2,
+      overwriteShortcut = {{"cmd", "alt"}, "2"}
+    },
+    {
+      listenShortcut = H3,
+      overwriteShortcut = {{"cmd", "alt"}, "3"}
+    },
+  },
+  Slack = { -- does not support H1,H2,H3
     {
       listenShortcut = INLINE_CODE_BLOCK,
       overwriteShortcut = {{"cmd", "shift"}, "C"}
@@ -45,6 +67,22 @@ local shortcutsConfig = {
     {
       listenShortcut = H1,
       overwriteShortcut = {{"ctrl", "shift"}, "1"}
+    },
+    {
+      listenShortcut = H2,
+      overwriteShortcut = {{"ctrl", "shift"}, "2"}
+    },
+    {
+      listenShortcut = H3,
+      overwriteShortcut = {{"ctrl", "shift"}, "3"}
+    },
+    {
+      listenShortcut = H3,
+      overwriteShortcut = {{"ctrl", "shift"}, "3"}
+    },
+    {
+      listenShortcut = CHECKBOX,
+      overwriteShortcut = {{"cmd", "shift"}, "7"}
     },
   },
   Asana = {
