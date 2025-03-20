250ed7049e03a092fdc5f3b54a2f19612e945c0e
JonTurner <JonTurner@users.noreply.github.com>
2025-03-19 20:10:44 -0400

    this hammerspoon config has been running well for the last 2+months

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,11 +1,3 @@
-local HYPER_KEY = {"cmd", "alt", "shift", "ctrl"}
-local HYPER_KEY_NO_SHIFT = {"cmd", "alt", "ctrl"}
-hs.hotkey.bind(HYPER_KEY, "W", function()
-  hs.alert.show("Hello World fooooooo1!")
-end)
-
-
---------------------------------------
 -- web-based (Linear, Slack, Notion, etc.) or Electron-based apps
 -- 1. Timing / Focus Issues in Web Apps (like Linear)
 --   For some apps, a short delay b/f keystroke will fix
@@ -20,29 +12,19 @@ end)
 --   ** this might be the answer to fix sometimes hotkeys don't work unless I reset focus on app
 --   set up global hotkeys
 --------------------------------------
--- Commonly Used "Listen" Shortcuts --
---------------------------------------
--- Press cmd/ctrl + option/shift + 0 to create text.
--- Notion shortcuts
--- local H1 = {{"cmd", "alt"}, "1"} -- Asana,
--- local H2 = {{"cmd", "alt"}, "2"}
--- local H3 = {{"cmd", "alt"}, "3"}
--- local CHECKBOX = {{"cmd", "alt"}, "4"}
--- local BULLET_LIST = {{"cmd", "alt"}, "5"}
--- local NUMBERED_LIST = {{"cmd", "alt"}, "6"}
--- local TOGGLE_LIST = {{"cmd", "alt"}, "7"} - creates an accordian - unique to Notion
--- local CODE_BLOCK = {{"cmd", "alt"}, "8"}
--- local COMMENT = {{"cmd", "shift"}, "M"} - for inline comment - Linear does not have inline comments..
--- local STRIKETHROUGH = {{"cmd", "shift"}, "S"}
+
+local HYPER_KEY = {"cmd", "alt", "shift", "ctrl"}
+local HYPER_KEY_NO_SHIFT = {"cmd", "alt", "ctrl"}
+-- hs.hotkey.bind(HYPER_KEY, "W", function()
+--   hs.alert.show("Hello World fooooooo1!")
+-- end)
 
 
--- notion shortcuts
-local INLINE_CODE_BLOCK = {HYPER_KEY_NO_SHIFT, "6"} -- except: Reflect
-local COPY_URL = {HYPER_KEY_NO_SHIFT, "1"} -- only: Notion, Linear    except: Reflect, Slack, Asana
--- google chrome and Firefox - cmd + L takes you to address bar w/ URL selected.  Just need cmd + C
 -- Linear: cmd + shift + . copies the branch name to your clipboard
 
 -- made up shortcuts to hopefully avoid collisions
+local INLINE_CODE_BLOCK = {HYPER_KEY_NO_SHIFT, "6"} -- except: Reflect
+local COPY_URL = {HYPER_KEY_NO_SHIFT, "1"} -- only: Notion, Linear    except: Reflect, Slack, Asana
 local H1 = {HYPER_KEY, "1"} -- except: Slack
 local H2 = {HYPER_KEY, "2"} -- except: Slack
 local H3 = {HYPER_KEY, "3"} -- except: Slack,Asana
@@ -52,8 +34,8 @@ local BULLET_LIST ={HYPER_KEY_NO_SHIFT, "8"} -- :all_applications!!!
 local CODE_BLOCK = {HYPER_KEY, "8"} -- except: reflect
 local NUMBERED_LIST = {HYPER_KEY, "6"} -- :all_applications!!!
 local HIGHLIGHT = {HYPER_KEY, "5"} -- natively supported on Notion & Reflect (not supported: Slack, Asana, Linear)
--- local HIGHLIGHT = {{"cmd", "shift"}, "H"} - natively supported on Notion & Reflect (not supported: Slack, Asana, Linear)
 
+-- Native Shortcuts that we are going to override
 local NOTION_COPY_URL = {{"cmd"}, "L"}
 local NOTION_H1 = {{"cmd", "alt"}, "1", 2}
 local NOTION_H2 = {{"cmd", "alt"}, "2", 2}
@@ -79,7 +61,7 @@ local LINEAR_H3 = {{"ctrl", "shift"}, "3", 2}
 local LINEAR_INLINE_CODE_BLOCK = {{"cmd"}, "E"}
 local LINEAR_CHECKBOX = {{"cmd", "shift"}, "7"}
 local LINEAR_STRIKETHROUGH = {{"cmd"}, "S", 2}
-local LINEAR_BULLET_LIST ={{"cmd", "shift"}, "8"}
+local LINEAR_BULLET_LIST ={{"cmd", "shift"}, "8", 2}
 local LINEAR_CODE_BLOCK = {{"cmd", "shift"}, "\\"}
 local LINEAR_NUMBERED_LIST = {{"cmd", "shift"}, "9"}
 
@@ -103,10 +85,6 @@ local REFLECT_HIGHLIGHT = {{"cmd", "shift"}, "H"}
 local CHROME_COPY_URL = {{"cmd"}, "L", 2}
 
 local FIREFOX_COPY_URL = {{"cmd"}, "L"}
--- Sample config table. Each key is an application name, and the value is a list of shortcuts.
--- "listenShortcut" is the keystroke you want Hammerspoon to listen for;
--- "nativeShortcut" is what Hammerspoon sends to the app.
--- Add more as needed…
 
 local shortcutsConfig = {
   Notion ={
@@ -168,42 +146,6 @@ local shortcutsConfig = {
     { listenShortcut = COPY_URL, nativeShortcut = FIREFOX_COPY_URL },
   },
 }
-
--- For each app in the table, we create a window filter that “turns on” hotkeys
--- when the app is focused and “turns them off” when you switch away.
--- for appName, shortcuts in pairs(shortcutsConfig) do
---   -- Create a window filter for just this one app
---   local appFilter = hs.window.filter.new(false):setAppFilter(appName)
---   local hotkeys = {}  -- We'll store the hotkey objects so we can remove them later
---
---   -- When the app is focused, bind the relevant hotkeys
---   appFilter:subscribe(hs.window.filter.windowFocused, function()
---     for _, shortcutItem in ipairs(shortcuts) do
---       local listenMods, listenKey = table.unpack(shortcutItem.listenShortcut)
---       local sendMods, sendKey, times = table.unpack(shortcutItem.nativeShortcut)
---       times = times or 1
---
---       -- Bind the hotkey
---       local h = hs.hotkey.bind(listenMods, listenKey, function()
---         hs.timer.usleep(50000)  -- 50ms delay
---         -- hs.alert.show("bind -> " .. listenKey)
---         for i = 1, times do
---           print("bind -> " .. listenKey)
---           hs.eventtap.keyStroke(sendMods, sendKey)
---         end
---       end)
---       table.insert(hotkeys, h)
---     end
---   end)
---
---   -- When the app is unfocused, disable and delete all the hotkeys we just created
---   appFilter:subscribe(hs.window.filter.windowUnfocused, function()
---     for _, h in ipairs(hotkeys) do
---       h:delete()
---     end
---     hotkeys = {}
---   end)
--- end
 
 --------------------------------------------
 -- Flatten "shortcutsConfig" into a table --
